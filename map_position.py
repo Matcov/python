6618d00bc508e71443b57f241dd02b2481573761
Alexei Matcov <matskov@yahoo.fr>
2023-09-15 11:10:55 +0300

    Create map_position.py

---
+++ map_position.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+# 🚨 Don't change the code below 👇
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+digits = position.split ()
+
+first_digit = int (digits [0][0])
+second_digit = int (digits [0][1])
+
+# first_digit is the row number, second_digit is the position in the row
+
+map   [second_digit-1] [first_digit-1] = 'X'
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
+
+
